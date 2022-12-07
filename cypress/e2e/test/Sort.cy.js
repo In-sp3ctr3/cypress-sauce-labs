c0a7212c75ca5e183ba70003ab381a5f75d234cd
Jadan <jadan@Jadans-Mac.local>
2022-12-07 00:39:11 -0500

    fix: headed command || sort update

--- cypress/e2e/test/Sort.cy.js
+++ cypress/e2e/test/Sort.cy.js
@@ -1,4 +1,3 @@
-const ProductsPage = require("../page/products.page");
 const LoginPage = require("../page/login.page");
 const LoginData = require("../testData/login.data");
 
@@ -8,23 +7,53 @@ describe("Sort", () => {
         LoginPage.login(LoginData.validUser.username, LoginData.validUser.password);
     });
     
-    it("should sort products by name", () => {
-        ProductsPage.sortProductsByName();
-        ProductsPage.productNames.should("have.length", 6);
-        ProductsPage.productNames.should("be.ordered", "asc");
-    });
-    
-    it("should sort products by price", () => {
-        ProductsPage.sortProductsByPrice();
-        ProductsPage.productNames.should("have.length", 6);
-        ProductsPage.productPrices.should("be.ordered", "asc");
-    });
+    it('should sort product list from A-Z', () => {
+        cy.get('.product_sort_container').select('az')
 
-    it ("should sort products by name in descending order", () => {
-        ProductsPage.sortProductsByName();
-        ProductsPage.sortProductsByName();
-        ProductsPage.productNames.should("have.length", 6);
-        ProductsPage.productNames.should("be.ordered", "desc");
-    }
-    );
+        var productList = ['Sauce Labs Backpack', 'Sauce Labs Bike Light', 'Sauce Labs Bolt T-Shirt', 'Sauce Labs Fleece Jacket', 'Sauce Labs Onesie', 'Test.allTheThings() T-Shirt (Red)']
+        productList.sort()
+
+        cy.get('.inventory_item_name').each(($elem, index) => {
+            expect($elem.text()).equal(productList[index])
+        })
+    })
+
+    it('should sort product list from Z-A', () => {
+        cy.get('.product_sort_container').select('za')
+
+        var productList = ['Sauce Labs Backpack', 'Sauce Labs Bike Light', 'Sauce Labs Bolt T-Shirt', 'Sauce Labs Fleece Jacket', 'Sauce Labs Onesie', 'Test.allTheThings() T-Shirt (Red)']
+        productList.sort().reverse()
+
+        cy.get('.inventory_item_name').each(($elem, index) => {
+            expect($elem.text()).equal(productList[index])
+        })
+    })
+
+    it('should sort product list from low to high', () => {
+        cy.get('.product_sort_container').select('lohi')
+
+        var productList = [['Sauce Labs Backpack', 29.99], ['Sauce Labs Bike Light', 9.99], ['Sauce Labs Bolt T-Shirt', 15.99], ['Sauce Labs Fleece Jacket', 49.99], ['Sauce Labs Onesie', 7.99], ['Test.allTheThings() T-Shirt (Red)', 15.99]]
+        productList.sort(function(a, b) {
+            return a[1] - b[1]
+        }
+        )
+
+        cy.get('.inventory_item_name').each(($elem, index) => {
+            expect($elem.text()).equal(productList[index][0])
+        }
+        )
+    })
+
+    it('should sort product list from high to low', () => {
+        cy.get('.product_sort_container').select('hilo')
+
+        var productList = [['Sauce Labs Backpack', 29.99], ['Sauce Labs Bike Light', 9.99], ['Sauce Labs Bolt T-Shirt', 15.99], ['Sauce Labs Fleece Jacket', 49.99], ['Sauce Labs Onesie', 7.99], ['Test.allTheThings() T-Shirt (Red)', 15.99]]
+        productList.sort(function(a, b) {
+            return b[1] - a[1]
+        })
+
+        cy.get('.inventory_item_name').each(($elem, index) => {
+            expect($elem.text()).equal(productList[index][0])
+        })
+    })
 });
